dfa90f0b1e605a5ecdc5621accdce8c68f56f39b
Ryan Jenkinson <ryancjenkinson@gmail.com>
2023-02-06 22:39:58 +0000

    refactor: :art: Remove the need for global function import
    The function was poorly named, we should rename it to be an internal
    function to remove the need of having to specify `global` within
    the main function. This might prevent memory leakages, but unsure.
    
    I never liked it there anyway, so best remove it!

--- tests/demand_flexibility_service/test_app.py
+++ tests/demand_flexibility_service/test_app.py
@@ -1,6 +1,6 @@
 import pytest
 
-from consumer_flex_app.demand_flexibility_service.app import get_previous_dfs_date
+from consumer_flex_app.demand_flexibility_service.app import _get_previous_dfs_date
 
 
 @pytest.fixture
@@ -11,10 +11,10 @@ def fake_dfs_dates() -> list[str]:
 class TestGetPreviousDFSDate:
     def test_get_previous_dfs_date_returns_previous_value_in_list(self, fake_dfs_dates):
         for dfs_date, previous_dfs_date in zip(fake_dfs_dates[1:], fake_dfs_dates[:-1]):
-            assert get_previous_dfs_date(dfs_date, fake_dfs_dates) == previous_dfs_date
+            assert _get_previous_dfs_date(dfs_date, fake_dfs_dates) == previous_dfs_date
 
     def test_get_previous_dfs_date_returns_initial_value_on_first_date(
         self, fake_dfs_dates
     ):
         first_dfs_date = fake_dfs_dates[0]
-        assert get_previous_dfs_date(first_dfs_date, fake_dfs_dates) == first_dfs_date
+        assert _get_previous_dfs_date(first_dfs_date, fake_dfs_dates) == first_dfs_date
